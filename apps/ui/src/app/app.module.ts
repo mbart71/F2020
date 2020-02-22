bdd51dd3bf86050c201a1cd5bf3c001321ae5b42
Flemming Bregnvig <flemming@bregnvig.dk>
2020-02-22 10:48:43 +0100

    Add PWA to web app

--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
