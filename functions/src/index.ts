a5d0a2e9c271ded3cb058320384b463205fd1db9
Flemming Bregnvig <flemming@bregnvig.dk>
2020-03-31 06:30:39 +0200

    Have to remove JS files...

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  firebase.initializeApp();
+  firebase.credential;
+  // console.log('sdfdsfdsfdsfsfd', firebase.auth().);
+  
+ response.send("Hello from Firebase!");
+});
