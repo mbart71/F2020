2de4a93fb3901ba170329e85341dc9395792cf19
Flemming Bregnvig <flemming@bregnvig.dk>
2020-04-04 10:03:52 +0200

    Add money transfer when submitting bid

---
+++ functions/src/lib/user.service.ts
@@ -0,0 +1,46 @@
+import * as admin from 'firebase-admin';
+import { converter } from './auth.converter';
+import { PlayerImpl } from './auth.model';
+import { getUser } from './auth.service';
+import { logAndCreateError } from './firestore-utils';
+import { Role } from './model/player.model';
+
+export const validateAccess = async (uid: string | undefined, ...role: Role[]): Promise<PlayerImpl> => {
+  if (uid) {
+    const player: PlayerImpl | undefined = await getUser(uid);
+
+    if (!player) {
+      throw logAndCreateError('permission-denied', `${uid} tried to login. No user with specified uid exists`);
+    }
+    
+    if (!player.isInRole('player')) {
+      throw logAndCreateError('permission-denied', `${player.displayName} does not have sufficient permissions. Role 'player' required. Has ${player.roles.join(', ')} `)
+    }
+    return player;
+  }
+  throw logAndCreateError('permission-denied', `No user was apparently logged in`);
+}
+
+
+let _bookie: Promise<PlayerImpl> | null = null;
+
+export const getBookie = (): Promise<PlayerImpl> => {
+  if (_bookie) {
+    return _bookie;
+  }
+  _bookie = admin.firestore().collection('player')
+    .where('roles', 'array-contains', 'bookie')
+    .withConverter(converter)
+    .get()
+    .then(({docs}) => {
+      if (docs.length === 1) {
+         return docs[0].data()
+      }
+      if (!docs.length) {
+        throw logAndCreateError('not-found', 'No bookie was found');
+      }
+      throw logAndCreateError('internal', `${docs.length} bookies exists. Only one bookie allowed`);
+    });
+  return _bookie;
+}
+
