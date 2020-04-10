bb6dccdac4e57711a48bd95fd951e137c7d73d08
Flemming Bregnvig <flemming@bregnvig.dk>
2020-04-10 15:42:46 +0200

    Updates test data

---
+++ functions/src/test-utils/players.collection.ts
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    roles: [ 'bookie' ],
+    email: 'bookie@bookie.dk',
+    balance: -4,
+    uid: 'CIQTphNeEfMXGUftQfoKvQt3lp73',
+    displayName: 'F1 Bookie'
+  },
+  {
+    balance: 1699,
+    uid: 'i18w2Ol5jqQVxUz11VZbiC1Mlqp2',
+    displayName: 'Flemming',
+    roles: [ 'admin', 'player' ],
+    email: 'flemming@flemming.dk'
+  },
+  {
+    roles: [ 'player', 'bank-admin' ],
+    email: 'michael@michael.dk',
+    uid: 'tdaLwa33t9gZ2n3rTbmQMW7CgbT2',
+    balance: 1569,
+    displayName: 'Michael'
+  }
+];
