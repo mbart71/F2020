f45ca6bb6620dc009cb61defc6eb741a415f9b8e
Flemming Bregnvig <flemming@bregnvig.dk>
2020-05-24 10:17:51 +0200

    Preparing for production

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCz1m3SvBz1OpVWLFWX-FL-SCY_8HbgxEE",
+    authDomain: "f1-2020-9dec0.firebaseapp.com",
+    databaseURL: "https://f1-2020-9dec0.firebaseio.com",
+    projectId: "f1-2020-9dec0",
+    storageBucket: "f1-2020-9dec0.appspot.com",
+    messagingSenderId: "209105975677",
+    appId: "1:209105975677:web:a18d0a67d142e0e85f3414",
+    measurementId: "G-BRDT1FLJ41"
+  }
 };
