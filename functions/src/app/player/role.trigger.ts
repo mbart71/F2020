7bf351cb035d127cd849e6ccf8074aa26cfbaefb
Michael Bartrup <michael.bartrup@gmail.com>
2020-05-30 14:24:18 +0200

    corrections to SetDefaultRoles

--- functions/src/app/player/role.trigger.ts
+++ functions/src/app/player/role.trigger.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 
 
-export const setDefaultRolese = functions.region('europe-west1').firestore.document('players/{userId}')
+export const setAnonymousRole = functions.region('europe-west1').firestore.document('players/{userId}')
     .onCreate(async (snap, context) => {  
      const newUser:any = snap.data();
      console.log(newUser?.displayName,' with uid ', newUser?.uid, 'has signed up, assigning a defaultrole');   
